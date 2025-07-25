b1bb18e93192bf6120bb794f5c5a124e9eca2aa3
mitaliborad <mitaliia109@gmail.com>
2025-07-25 12:03:56 +0530

    Multiple If Statements in Succession

--- rolercoster.py
+++ rolercoster.py
@@ -3,11 +3,25 @@ height = int(input("Please Enter Your height in cm : "))
 
 if height >= 120:
     print("You can ride RollerCoaster.")
+    bill = 0 
 
     age = int(input("Enter your age : "))
-    if age <= 18:
-        print("You have to pay only $7 for ticket.")
+    if age <=12 :
+        bill = 5
+        print("child tickets are $5.")
+    elif age<=18:
+        bill = 7
+        print("youth tickets are $7.")
     else:
-        print("Your ticket is $12.")
+        bill = 12
+        print("Adult tickets are $12.")
+     
+    wants_photo = input("Do you want to have a photo take? Type y for Yes and n for No.")   
+    if wants_photo == "y" :
+        bill += 3
+    
+    print(f"your final bill is ${bill}")
+   
+        
 else : 
     print("Sorry! You have to grow taller before you can ride.")
