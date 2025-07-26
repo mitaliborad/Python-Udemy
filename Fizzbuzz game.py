438b0f2475b9a9a62b9c0520bee91f7fceeaf33a
mitaliborad <mitaliia109@gmail.com>
2025-07-26 14:45:51 +0530

    Coding Exercise : FizzBuzz

---
+++ Fizzbuzz game.py
@@ -0,0 +1,27 @@
+# total = 0
+# for number in range(1,101):
+#     total += number
+# print(total)
+
+
+
+# FizzBuzz
+# You are going to write a program that automatically prints the solution to the FizzBuzz game. These are the rules of the FizzBuzz game:
+# Your program should print each number from 1 to 100 in turn and include number 100.
+# But when the number is divisible by 3 then instead of printing the number it should print "Fizz".
+# When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
+# And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
+# e.g. it might start off like this:
+
+print("welcome to Fizzbuzz game")
+for number in range(1,101):
+    if number%3==0:
+        print("Fuzz")
+    elif number%5==0:
+        print("Buzz")
+    elif number%3==0 and number/5==0:
+        print("FizzBuzz")
+    else:
+        print(number)
+        
+        
